fa5711aca95dd77a2b6e4bae4d9c39e0e56a67ad
Leesin0222 <dragon02229@naver.com>
2021-12-28 09:39:33 +0900

    📑 :: MainActivity

--- ViewModelLiveData/app/src/main/java/com/yongjincompany/viewmodellivedata/MainActivity.kt
+++ ViewModelLiveData/app/src/main/java/com/yongjincompany/viewmodellivedata/MainActivity.kt
@@ -2,10 +2,39 @@ package com.yongjincompany.viewmodellivedata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.yongjincompany.viewmodellivedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: TestViewModel
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(TestViewModel::class.java)
+
+        viewModel.currentNumber.observe(this, Observer {
+            binding.tvTextView.text = it.toString()
+        })
+
+        viewModel.currentBoolean.observe(this, Observer {
+            binding.tvBooleanText.text = it.toString()
+        })
+
+        incrementText()
+    }
+
+    private fun incrementText() {
+        binding.btnButton.setOnClickListener {
+            viewModel.currentNumber.value = ++viewModel.number
+            viewModel.currentBoolean.value = viewModel.number % 2 == 0
+
+        }
     }
 }
